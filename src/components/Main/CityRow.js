761c29fca1d1b2c07aff64150ff77b6b65442878
Hadar Alon <hadaralon3@gmail.com>
2020-05-17 17:37:18 +0300

    updates

--- src/components/Main/CityRow.js
+++ src/components/Main/CityRow.js
@@ -3,19 +3,32 @@ import { observer, inject } from 'mobx-react'
 import "../../styles/components/cityRow.scss"
 
 const CityRow = inject("CityStore", "HeaderStore")(observer(function (props) {
+const error_text = "The allowed number of requests has been exceeded, please try again later"
 
-        return (
-            <div className="citySection">
-                <div className="icon_temp">
-                    <div><img alt="weather" src={props.CityStore.iconsFunc(props.CityStore.city.icon)} className="weatherPhoto" /></div>
-                    <div className="citySection_temp">{props.HeaderStore.celsiusType ? props.CityStore.city.currentTemp + "°C" : props.HeaderStore.celToFer(props.CityStore.city.currentTemp)}</div>
+        return (   
+           
+                props.CityStore.city.name ? 
+
+               <div className="citySection">
+                 <div className="icon_temp">
+                 <div><img alt="weather" src={props.CityStore.iconsFunc(props.CityStore.city.icon)} className="weatherPhoto" /></div>
+                 <div className="citySection_temp">{props.HeaderStore.celsiusType ? props.CityStore.city.currentTemp + "°C" : props.HeaderStore.celToFer(props.CityStore.city.currentTemp)}</div>
                 </div>
                 <div className="citySection_city_text">
                     <div className="city_name">{props.CityStore.city.name}</div>
                     <div className="line"></div>
                     <div className="city_weather">{props.CityStore.city.weatherText}</div>
                 </div>
-            </div>
+
+                </div>
+
+                :
+
+                 <div className="error__text">{error_text}</div>
+            
+            
+                
+          
         );  
 }))
 
